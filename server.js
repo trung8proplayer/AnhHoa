8d0b96e4d3156a03be446a9ff64b7c03c972735f
Trung <trung4416180@gmail.com>
2021-11-14 15:07:59 +0700

    deploy3

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
